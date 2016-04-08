8da209802bbc3a2b02e7ceb9d286f0d4de8e2a90
Larry Adames <adames.larry@gmail.com>
2016-04-08 14:52:59 -0400

    set up gems and rspec

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+ruby "2.2.1"
 
 gem 'rails', '4.2.5'
 gem 'pg', '~> 0.15'
@@ -9,13 +9,31 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'rails_12factor', group: :production
+gem 'quiet_assets'
+gem 'haml', '~> 4.0', '>= 4.0.7'
+gem 'devise'
 
 group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'faker'
   gem 'byebug'
+  gem 'pry'
 end
 
 group :development do
   gem 'web-console', '~> 2.0'
   gem 'spring'
+end
+
+
+group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'shoulda-matchers', '~> 3.1'
+  gem 'selenium-webdriver'
+  gem "chromedriver-helper"
 end
 
---
+++ spec/rails_helper.rb
@@ -0,0 +1,55 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require 'rspec/rails'
+# require 'rspec/autorun'
+require 'database_cleaner'
+require 'capybara/rspec'
+require 'shoulda/matchers'
+require_relative 'support/controller_helpers'
+require 'devise'
+require 'spec_helper'
+require 'factory_girl_rails'
+require 'delegate'
+require 'active_support/concern'
+
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+Capybara.app_host = "http://example.com"
+
+RSpec.configure do |config|
+
+  config.include ControllerHelpers, type: :controller
+  config.include Warden::Test::Helpers
+  config.include FactoryGirl::Syntax::Methods
+  config.include Devise::TestHelpers, type: :controller
+
+  Warden.test_mode!
+
+  config.after do
+    Warden.test_reset!
+  end
+  config.order = 'random'
+  config.include Rails.application.routes.url_helpers
+  config.include Capybara::DSL
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      # Choose a test framework:
+      with.test_framework :rspec
+
+      # Choose one or more libraries:
+      with.library :active_record
+      with.library :active_model
+      with.library :action_controller
+      # Or, choose the following (which implies all of the above):
+      with.library :rails
+    end
+  end
+end
