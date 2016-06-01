6f9882e18005c63bfbf4e64c1b36270d574c2480
Jimmy Adames <jimmy.adames@gmail.com>
2016-06-01 15:56:13 -0400

    added smtp configuration for mailgun

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.mailgun.org',
-  :port => '587',
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'voices-beta.heroku.com',
   :authentication => :plain,
-  :user_name => ENV['MAILGUN_SMTP_LOGIN'],
-  :password => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain => 'heroku.com',
-  :enable_starttls_auto => true
 }
+ActionMailer::Base.delivery_method = :smtp
